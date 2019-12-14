41146542e2f6d44a1d79491222952b374b447377
Yu Nakata <y-nakata@outlook.com>
2019-12-14 10:06:53 +0000

    混雑度のget APIを追加

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.listen(8001);
 
+var congestion_tables = {};
+
 app.post('/', function(req, res) {
-    console.log("ドアID: "+ req.body.door_id + ", 混雑度: " + req.body.conjestion);
+    console.log("ドアID: "+ req.body.door_id + ", 混雑度: " + req.body.congestion);
+    congestion_tables[req.body.door_id] = req.body.congestion;
     res.send('POST is sended.');
+})
+
+app.get('/congestions', function(req, res) {
+    console.log("Query congestions");
+    console.log(congestion_tables);
+    res.send(JSON.stringify(congestion_tables));
 })
