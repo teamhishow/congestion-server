7dd7787132b5edf8003036eb06b97e3836f4c951
Yu Nakata <y-nakata@outlook.com>
2019-12-14 15:51:27 +0000

    保存した混雑度で色を表示

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.listen(8001);
 var congestion_tables = {};
 
 congestion_tables = Array(10).fill().map(() => Array(4).fill(0));
-console.log(congestion_tables);
+//console.log(congestion_tables);
 
 app.post('/', function(req, res) {
     console.log("車両: " + req.body.car_id +
@@ -32,10 +32,17 @@ app.get('/congestions', function(req, res) {
 })
 
 app.get('/', function(req, res) {
-    var gb = Math.min(20, Math.round(255 - 255 * (655 / 1000.0)));
-    console.log(gb);
-    var rgb = "ff" + componentToHex(gb) + componentToHex(gb);
-    console.log(rgb);
+    var congestion_rgb = congestion_tables;
+    for(const car_congestion_index in congestion_tables) {
+        const car_congestion = congestion_tables[car_congestion_index];
+        for(const door_congestion_index in car_congestion) {
+            const door_congestion = car_congestion[door_congestion_index];
+            var gb = Math.min(20, Math.round(255 - 255 * (door_congestion / 1000.0)));
+            console.log(gb);
+            var rgb = "ff" + componentToHex(gb) + componentToHex(gb);
+            console.log(rgb);
+        }
+    }
     res.render("index", {congestions: [[rgb, "ff4500"], ["ff6347", "ff0000"]]});
 })
 
