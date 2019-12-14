da987d714385bf49ce82f6b3d6eba6fd8a20e12d
Yu Nakata <y-nakata@outlook.com>
2019-12-14 15:13:59 +0000

    混雑度に応じて色が変わる

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const max_congestion = 1000;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
@@ -11,9 +13,15 @@ app.listen(8001);
 
 var congestion_tables = {};
 
+congestion_tables = Array(10).fill().map(() => Array(4).fill(0));
+console.log(congestion_tables);
+
 app.post('/', function(req, res) {
-    console.log("ドアID: "+ req.body.door_id + ", 混雑度: " + req.body.congestion + ", 時刻: " + req.body.time);
-    congestion_tables[req.body.door_id] = req.body.congestion;
+    console.log("車両: " + req.body.car_id +
+        "ドアID: "+ req.body.door_id +
+        ", 混雑度: " + req.body.congestion +
+        ", 時刻: " + req.body.time);
+    congestion_tables[req.body.car_id][req.body.door_id] = req.body.congestion;
     res.send('POST is sended.');
 })
 
@@ -24,8 +32,16 @@ app.get('/congestions', function(req, res) {
 })
 
 app.get('/', function(req, res) {
-    const message = "Hello World!";
-    res.render("index", {congestions: [["ff0000", "ff4500"], ["ff6347", "ff0000"]], message: message});
+    var gb = Math.round(255 - 255 * (655 / 1000.0));
+    console.log(gb);
+    var rgb = "ff" + componentToHex(gb) + componentToHex(gb);
+    console.log(rgb);
+    res.render("index", {congestions: [[rgb, "ff4500"], ["ff6347", "ff0000"]]});
 })
 
 app.use(express.static(path.join(__dirname, 'views')));
+
+function componentToHex(c) {
+      var hex = c.toString(16);
+      return hex.length == 1 ? "0" + hex : hex;
+}
